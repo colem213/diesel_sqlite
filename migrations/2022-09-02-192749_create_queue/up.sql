9344654c62d92b9939e0e356fc03db2d83dcba9d
Marc Coleman <mcoleman213@protonmail.com>
2022-09-02 20:02:49 -0400

    Change src/schema.rs type to TimestamptzSqlite

--- migrations/2022-09-02-192749_create_queue/up.sql
+++ migrations/2022-09-02-192749_create_queue/up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE queue (
   id TEXT PRIMARY KEY NOT NULL,
-  created_at TEXT NOT NULL,
-  updated_at TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
 
-  scheduled_for TEXT NOT NULL,
+  scheduled_for TIMESTAMP NOT NULL,
   failed_attempts INT NOT NULL,
   status INT NOT NULL,
   message TEXT NOT NULL
--- src/schema.rs
+++ src/schema.rs
@@ -3,9 +3,9 @@
 diesel::table! {
     queue (id) {
         id -> Text,
-        created_at -> Text,
-        updated_at -> Text,
-        scheduled_for -> Text,
+        created_at -> TimestamptzSqlite,
+        updated_at -> TimestamptzSqlite,
+        scheduled_for -> TimestamptzSqlite,
         failed_attempts -> Integer,
         status -> Integer,
         message -> Text,
